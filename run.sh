89cc092c205e110dc54d2fefc6aa621b06fb95c8
Saurabh Goyal saurago1@in.ibm.com <saurago@dccxl001.pok.ibm.com>
2020-07-21 17:26:03 -0400

    Initial commit

--- run.sh
+++ run.sh
@@ -1,25 +1,20 @@
 
-#source activate /u/saurago/anaconda3/envs/tf
-source activate /dccstor/smraje/tf1_15/anaconda3/envs/tf
+export BERT_BASE_DIR=/path/to/bert/uncased_L-12_H-768_A-12
+export GLUE_DIR=/path/to/glue
+export OUTPUT_DIR=/path/to/output/directory
 
-CHECKPOINT_PATH='../anbang_sentiment_analysis/bert/data/uncased_L-12_H-768_A-12/bert_model.ckpt'
-DATA_DIR='../glue_data1/RTE'
-VOCAB_PATH='../anbang_sentiment_analysis/bert/data/uncased_L-12_H-768_A-12/vocab.txt'
-BERT_CONFIG_PATH='../anbang_sentiment_analysis/bert/data/uncased_L-12_H-768_A-12/bert_config.json'
-OUTPUT_DIR='./Output'
+mkdir -p $OUTPUT_DIR
 
-mkdir $OUTPUT_DIR
-
-python main.py  --TASK "rte" \
-	        --EPOCH 3 \
-		--BATCH_SIZE 16 \
-		--LR_SOFT_EXTRACT 0.0015 \
-		--LR_BERT 0.00002 \
-		--LAMBDA 0.007 \
-		--CHECKPOINT_PATH $CHECKPOINT_PATH \
-		--DATA_DIR $DATA_DIR \
-		--VOCAB_PATH $VOCAB_PATH \
-		--BERT_CONFIG_PATH $BERT_CONFIG_PATH \
-		--OUTPUT_DIR $OUTPUT_DIR
-
+python main.py --TASK "mrpc" \
+--BERT_CONFIG_PATH $BERT_BASE_DIR/bert_config.json  \
+--CHECKPOINT_PATH $BERT_BASE_DIR/bert_model.ckpt \
+--VOCAB_PATH $BERT_BASE_DIR/vocab.txt \
+--CASED \
+--DATA_DIR $GLUE_DIR/MRPC \
+--EPOCH 3 \
+--BATCH_SIZE 64 \
+--LR_SOFT_EXTRACT 0.003 \
+--LR_BERT 0.00006 \
+--LAMBDA 0.003 \
+--OUTPUT_DIR $OUTPUT_DIR 
 
